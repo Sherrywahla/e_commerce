e1c9980fce19ec8acc1db767eafd31863b2cbe68
Sherrywahla <shaheryarwahla8@gmail.com>
2024-11-26 19:40:38 +0300

    Feature tailwind - added path theme - theme to installed app - django browser reload to installed app - path for statics files directories

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -102,14 +102,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'theme', 'static_src'),
-#     os.path.join(BASE_DIR, 'theme', 'static')
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'theme', 'static_src'),
+    os.path.join(BASE_DIR, 'theme', 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
