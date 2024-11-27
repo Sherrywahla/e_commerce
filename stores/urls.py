d352f27a8296544690b430cf8faea639e1cf06eb
Sherrywahla <shaheryarwahla8@gmail.com>
2024-11-27 10:17:41 +0300

    Cart & orders - Place orders - cart.html - order_success.html - static files - header tiles fixes

--- stores/urls.py
+++ stores/urls.py
@@ -17,5 +17,7 @@ urlpatterns = [
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/remove/<int:cart_id>/', views.remove_from_cart, name='remove_from_cart'),
     
+    path('order/place/', views.place_order, name='place_order'),
+    
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
